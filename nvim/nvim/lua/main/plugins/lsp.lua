33fe9e6db2bd5d4d165687d2d1ab355b1ebba26d
rpop0 <38384209+rpop0@users.noreply.github.com>
2024-05-10 17:03:13 +0300

    change(lsp): Added lua_ls to ensure_installed

--- nvim/nvim/lua/main/plugins/lsp.lua
+++ nvim/nvim/lua/main/plugins/lsp.lua
@@ -61,7 +61,7 @@ lsp_config.config = function()
     end)
 
     require('mason-lspconfig').setup({
-        ensure_installed = { 'basedpyright', 'ruff', 'tsserver', 'cssls', 'html' },
+        ensure_installed = { 'basedpyright', 'ruff', 'tsserver', 'cssls', 'html', 'lua_ls' },
         handlers = {
             function(server_name)
                 require('lspconfig')[server_name].setup({})
