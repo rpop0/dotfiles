998360b77c881b794c4a4ff8f1afb51cd6bd460d
rpop0 <38384209+rpop0@users.noreply.github.com>
2024-06-08 15:04:41 +0300

    feat(nvim): Enable copilot only on work laptop

--- nvim/.config/nvim/lua/main/plugins/copilot.lua
+++ nvim/.config/nvim/lua/main/plugins/copilot.lua
@@ -2,6 +2,13 @@ return {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
     event = 'InsertEnter',
+    -- Only enable on work laptop.
+    cond = function ()
+        if vim.loop.os_gethostname() ~= "C-5CG2124ZCY" then
+            return false;
+        end
+        return true;
+    end,
     config = function ()
         require('copilot').setup({
             suggestion = {
