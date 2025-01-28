02d7285ca954e98af23fba441d9de4bb7be2d065
rpop0 <38384209+rpop0@users.noreply.github.com>
2025-01-29 01:09:31 +0200

    Add smooth cursor and dap fixes

--- nvim/dot-config/nvim/lua/main/plugins/dap.lua
+++ nvim/dot-config/nvim/lua/main/plugins/dap.lua
@@ -37,20 +37,9 @@ local debug_plugins = {
 
         })
 
-        -- dap.defaults.fallback.external_terminal = {
-        --     command = "tmux",
-        --     args = { "split-window", "-h", "-d", "-p", "35" }
-        -- }
-
         -- DAP UI config
         dap.listeners.after.event_initialized.dapui_config = function()
             dapui.open()
-        end
-        -- dap.listeners.before.event_terminated.dapui_config = function()
-            -- dapui.close()
-        -- end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
         end
 
         vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
