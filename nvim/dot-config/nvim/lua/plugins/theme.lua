e23c7c436e7d773f2138ba2fed72728486218457
rpop0 <38384209+rpop0@users.noreply.github.com>
2025-08-15 14:03:09 +0300

    [nvim] Make theme transparent

--- nvim/dot-config/nvim/lua/plugins/theme.lua
+++ nvim/dot-config/nvim/lua/plugins/theme.lua
@@ -5,7 +5,7 @@ return{
     priority = 1000,
     config = function()
         require("catppuccin").setup({
-            transparent_background = false,
+            transparent_background = true,
             integrations = {
                 blink_cmp = true,
                 treesitter = true,
