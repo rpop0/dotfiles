4b22c55fbf3b8244ee53492c56c0f6a026a5042a
rpop0 <38384209+rpop0@users.noreply.github.com>
2026-03-19 20:55:50 +0200

    chore(nvim): Additional basedpyright rules

--- nvim/dot-config/nvim/after/lsp/basedpyright.lua
+++ nvim/dot-config/nvim/after/lsp/basedpyright.lua
@@ -11,6 +11,8 @@ return {
                     reportUnusedVariable = 'none',
                     reportUndefinedVariable = 'none',
                     reportAny = 'none',
+                    reportUnusedCallResult = 'none',
+                    reportUnannotatedClassAttribute = 'none'
                 }
             }
 
