3d34ad132c74ccf31255118d8b409abfe421a257
rpop0 <38384209+rpop0@users.noreply.github.com>
2026-04-01 07:55:58 +0300

    feat(nvim): Supress basedpyright spam

--- nvim/dot-config/nvim/after/lsp/basedpyright.lua
+++ nvim/dot-config/nvim/after/lsp/basedpyright.lua
@@ -1,7 +1,4 @@
 return {
-    name = "basedpyright",
-    filetypes = { "python" },
-    cmd = { "basedpyright-langserver", "--stdio" },
     settings = {
         basedpyright = {
             disableOrganizeImports = true, -- ruff_lsp
@@ -11,11 +8,23 @@ return {
                     reportUnusedVariable = 'none',
                     reportUndefinedVariable = 'none',
                     reportAny = 'none',
+                    reportExplicitAny = 'none',
                     reportUnusedCallResult = 'none',
                     reportUnannotatedClassAttribute = 'none'
                 }
-            }
+            },
 
         }
-    }
+    },
+
+    handlers = {
+        --- filter noisy notifications
+        ['$/progress'] = function(err, result, ctx)
+            -- just notify once
+            if result.token == (vim.g.basedpyright_progress_token or result.token) then
+                vim.g.basedpyright_progress_token = result.token
+                vim.lsp.handlers['$/progress'](err, result, ctx)
+            end
+        end,
+    },
 }
