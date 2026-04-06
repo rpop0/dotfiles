b9a27d212b8d5c8089f410141fe6063a7d379662
rpop0 <38384209+rpop0@users.noreply.github.com>
2026-04-06 07:54:28 +0300

    feat(nvim): Add obsidian

---
+++ nvim/dot-config/nvim/plugin/obsidian.lua
@@ -0,0 +1,11 @@
+vim.pack.add({ { src = 'https://github.com/obsidian-nvim/obsidian.nvim', version = vim.version.range('*') } })
+
+require('obsidian').setup({
+    legacy_commands = false,
+    workspaces = {
+        {
+            name = 'work',
+            path = '~/vaults/work'
+        }
+    }
+})
